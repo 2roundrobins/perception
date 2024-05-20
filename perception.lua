c9c854ba4dd4dcfceb4caf50628d2d57b48cb6b2
2roundrobins <139110506+2roundrobins@users.noreply.github.com>
2024-05-20 17:05:24 +0200

    added a few things

--- perception.lua
+++ perception.lua
@@ -9,15 +9,14 @@
 --- load sample via PARAMS
 --- E1 change the animal 
 --- E2 volume
---- E3 detune (issue)
+--- E3 fine tune
 --- K1 toggle recording
 --- K2 start/stop
 --- K3 reverse
 
 
 -- TO DO
--- ISSUE: when recording from external audio it records also the sample playback, should be seperate! dont know why, there are different buffers
--- ISSUE: detuning not working properly, E3 gives me only two values, without inbetween, making the tuning not viable. 
+-- ISSUE: need to find a good way to show detuning in the cff value
 
 
 -- vars
@@ -31,9 +30,10 @@ sample_tune = 1
 sample_is_planted = false
 pos = 1
 rec = 0
-play = 0
+play = 1
 pre = 0.8
 k1_pressed = false
+shape_shifter_metro = metro.init()
 
 -- init
 function init()
@@ -52,9 +52,8 @@ function init()
     softcut.loop_end(sample_voice, MAX_BUFFER)
     softcut.position(sample_voice, 1)
     softcut.rate_slew_time(sample_voice, 4)
-    softcut.post_filter_rq(sample_voice, 10)
     softcut.play(sample_voice, 1)
-    softcut.rec(sample_voice, 0) -- just here to explicitly make sure that the rec flag is off
+    softcut.rec(sample_voice, 0)
 
     softcut.fade_time(sample_voice, 0)
     
@@ -62,7 +61,7 @@ function init()
     softcut.enable(rec_voice,1)
     softcut.buffer(rec_voice,1)
     softcut.level_input_cut(1,rec_voice,1.0)
-    softcut.level_input_cut(2,rec_voice,1.0) -- grab audio from both adc inputs
+    softcut.level_input_cut(2,rec_voice,1.0)
 
     softcut.level(rec_voice,1.0)
     softcut.rate(rec_voice,1.0)
@@ -76,17 +75,23 @@ function init()
     softcut.rate_slew_time(rec_voice, 4)
     softcut.post_filter_rq(rec_voice, 10)
 
-    softcut.play(rec_voice, 1) -- play flag should be on at all times. use softcut.level to mute and softcut.position to set position.
-    softcut.rec(rec_voice, 1) -- rec flag should be on at all times. modulate rec_level and pre_level to toggle recording.
-    softcut.rec_level(rec_voice,0) -- rec level 0 as default --> no recording.
-    softcut.pre_level(rec_voice,1) -- pre level 1 as default --> no erasing.
+    softcut.play(rec_voice, 1)
+    softcut.rec(rec_voice, 1)
+    softcut.rec_level(rec_voice,0)
+    softcut.pre_level(rec_voice,1) 
 
     setRate(animal_tab[current_animal].cff)
     setFreq()
+    stop_shape_shifter()
     
     
   -- sample parameters
-  params:add_separator("sample_params", "sample")
+  params:add_separator("rec_params", "Recording")
+  
+  params:add_control("rec_level", "intensity", controlspec.new(0, 1, 'lin', 0, 0.7), function(param) return (round_form(util.linlin(0, 1, 0, 100, param:get()), 1, "%")) end)
+  params:set_action("rec_level", function(val) rec_level = val softcut.level(rec_voice, val) end)
+  
+  params:add_separator("sample_params", "Sample")
   params:add_file("load_sample", "> select sample", "")
   params:set_action("load_sample", function(path) load_audio(path) end)
   
@@ -97,9 +102,26 @@ function init()
    params:add_option("plant_sample", "load?", {"no", "yes"}, 2)
   params:set_action("plant_sample", function(val) sample_is_planted = val == 2 and true or false toggle_sample() end)
   
-  --problem with detune
-  -- params:add_control("sample_tune", "detune", controlspec.new(-1, 1, 'lin', 0, setRate(animal_tab[current_animal].cff)), function(param) return (round_form(util.linlin(0, 1, 0, 100, param:get()), 1, "%")) end)
-  -- params:set_action("sample_tune", function(val) sample_tune = val setRate(sample_voice, val) end)
+  
+  params:add_separator("freq_params", "Hearing Range")
+  params:add_option("freq_view", "implement?", {"No", "Yes"}, 1)
+  params:set_action("freq_view", updateFreqView)
+  
+  
+  
+  params:add_separator("fine_tune", "Fine Tune")
+  params:add_number("detune_semitones", "Semitones", -12, 12, 0, function(param) return round_form(param:get(), 1, "st") end)
+  params:set_action("detune_semitones", function(semi) detune = semi / 12 update_rate() end)
+  params:add_number("detune", "Cents", -600, 600, 0, function(param) return (round_form(param:get(), 1, "cents")) end)
+  params:set_action("detune", function(cent) detune = cent / 1200 update_rate() end)
+  params:add{type = "text", id = "cff_display", name = "Current CFF: " .. animal_tab[current_animal].cff, action = function() end}
+  
+  params:add_separator("shape_shifter", "Shape Shifter")
+  params:add_option("shape_shifter", "Shape Shifter", {"off", "on"}, 1)
+  params:set_action("shape_shifter", function(value) if value == 2 then start_shape_shifter() else stop_shape_shifter() end end)
+  params:add_control("shape_shifter_interval", "Shape Shifter Interval", controlspec.new(1, 60, "lin", 1, 5, "s")) 
+  params:set_action("shape_shifter_interval", function(value) shape_shifter_metro.time = value if params:get("shape_shifter") == 2 then start_shape_shifter() end end)
+  
   
   params:bang()
 
@@ -109,21 +131,21 @@ end
 
 -- animal table
 animal_tab = {
-    {name = "Cane Toad", species = "Rhinella marina", order = "Anura", class = "Amphibia", phylum = "Chordata", lifespan = 12.5, cff = 6.7, lp = 3000, hp = 20},
-    {name = "Green Frog", species = "Rana clamitans", order = "Anura", class = "Amphibia", phylum = "Chordata", lifespan = 3, cff = 21, lp = 20000, hp = 20},
-    {name = "Rainbow Trout", species = "Oncorhynchus mykiss", order = "Salmoniformes", class = "Actinopterygii", phylum = "Chordata", lifespan = 3.5, cff = 27, lp = 500, hp = 100},
-    {name = "Harp Seal", species = "Pagophilus groenlandicus", order = "Carnivora", class = "Mammalia", phylum = "Chordata", lifespan = 32, cff = 33, lp = 20000, hp = 950},
-    {name = "Brown Rat", species = "Rattus norvegicus", order = "Rodentia", class = "Mammalia", phylum = "Chordata", lifespan = 2, cff = 39, lp = 20000, hp = 500},
-    {name = "Horned Owl", species = "Bubo virginianus", order = "Strigiformes", class = "Aves", phylum = "Chordata", lifespan = 25, cff = 45, lp = 12000, hp = 200},
-    {name = "Cat", species = "Felis catus", order = "Carnivora", class = "Mammalia", phylum = "Chordata", lifespan = 14, cff = 55, lp = 20000, hp = 45},
-    {name = "Human", species = "Homo sapiens", order = "Primates", class = "Mammalia", phylum = "Chordata", lifespan = 77, cff = 60, lp = 20000, hp = 20},
-    {name = "Tussah Moth", species = "Antheraea pernyi", order = "Lepidoptera", class = "Insecta", phylum = "Arthropoda", lifespan = 0.02, cff = 70, lp = 20000, hp = 10000},
-    {name = "Dog",species = "Canis lupus", order = "Carnivora", class = "Mammalia", phylum = "Chordata", lifespan = 13, cff = 80, lp = 20000, hp = 67},
-    {name = "Treeshrew", species = "Tupaia glis", order = "Scandentia", class = "Mammalia", phylum = "Chordata", lifespan = 2.5, cff = 90, lp = 6000, hp = 250},
-    {name = "Starling", species = "Sturnus vulgaris", order = "Passeriformes", class = "Aves", phylum = "Chordata", lifespan = 2.5, cff = 100, lp = 8700, hp = 700},
-    {name = "Ground Squirrel", species = "Spermophilus lateralis", order = "Rodentia", class = "Mammalia", phylum = "Chordata", lifespan = 7, cff = 120, lp = 32000, hp = 500},
-    {name = "Tsetse Fly", species = "Glossina morsitans", order = "Diptera", class = "Insecta", phylum = "Arthropoda", lifespan = 0.08, cff = 145, lp = 7200, hp = 5300},
-    {name = "Honey Bee", species = "Apis mellifera", order = "Hymenoptera", class = "Insecta", phylum = "Arthropoda", lifespan = 0.1, cff = 200, lp = 500, hp = 100}
+    {name = "Cane Toad", species = "Rhinella marina", order = "Anura", class = "Amphibia", phylum = "Chordata", lifespan = 12.5, cff = 6.7, lp = 3000, hp = 20, bp = 245, q = 10},
+    {name = "Green Frog", species = "Rana clamitans", order = "Anura", class = "Amphibia", phylum = "Chordata", lifespan = 3, cff = 21, lp = 20000, hp = 20, bp = 10010, q = 0.1},
+    {name = "Rainbow Trout", species = "Oncorhynchus mykiss", order = "Salmoniformes", class = "Actinopterygii", phylum = "Chordata", lifespan = 3.5, cff = 27, lp = 500, hp = 100, bp = 223, q = 20},
+    {name = "Harp Seal", species = "Pagophilus groenlandicus", order = "Carnivora", class = "Mammalia", phylum = "Chordata", lifespan = 32, cff = 33, lp = 20000, hp = 950, bp = 4356, q = 2},
+    {name = "Brown Rat", species = "Rattus norvegicus", order = "Rodentia", class = "Mammalia", phylum = "Chordata", lifespan = 2, cff = 39, lp = 20000, hp = 500, bp = 3162, q = 2},
+    {name = "Horned Owl", species = "Bubo virginianus", order = "Strigiformes", class = "Aves", phylum = "Chordata", lifespan = 25, cff = 45, lp = 12000, hp = 200, bp = 1650, q = 2},
+    {name = "Cat", species = "Felis catus", order = "Carnivora", class = "Mammalia", phylum = "Chordata", lifespan = 14, cff = 55, lp = 20000, hp = 45, bp = 2000, q = 2},
+    {name = "Human", species = "Homo sapiens", order = "Primates", class = "Mammalia", phylum = "Chordata", lifespan = 77, cff = 60, lp = 20000, hp = 20, bp = 2000, q = 5},
+    {name = "Tussah Moth", species = "Antheraea pernyi", order = "Lepidoptera", class = "Insecta", phylum = "Arthropoda", lifespan = 0.02, cff = 70, lp = 20000, hp = 10000, bp = 4472, q = 5},
+    {name = "Dog",species = "Canis lupus", order = "Carnivora", class = "Mammalia", phylum = "Chordata", lifespan = 13, cff = 80, lp = 20000, hp = 67, bp = 2000, q = 5},
+    {name = "Treeshrew", species = "Tupaia glis", order = "Scandentia", class = "Mammalia", phylum = "Chordata", lifespan = 2.5, cff = 90, lp = 6000, hp = 250, bp = 1224, q = 2},
+    {name = "Starling", species = "Sturnus vulgaris", order = "Passeriformes", class = "Aves", phylum = "Chordata", lifespan = 2.5, cff = 100, lp = 8700, hp = 700, bp = 2466, q = 5},
+    {name = "Ground Squirrel", species = "Spermophilus lateralis", order = "Rodentia", class = "Mammalia", phylum = "Chordata", lifespan = 7, cff = 120, lp = 32000, hp = 50, bp = 2000, q = 0.1},
+    {name = "Tsetse Fly", species = "Glossina morsitans", order = "Diptera", class = "Insecta", phylum = "Arthropoda", lifespan = 0.08, cff = 145, lp = 7200, hp = 5300, bp = 6175, q = 5},
+    {name = "Honey Bee", species = "Apis mellifera", order = "Hymenoptera", class = "Insecta", phylum = "Arthropoda", lifespan = 0.1, cff = 200, lp = 500, hp = 100, bp = 250, q = 8}
 }
 
 --cff rate
@@ -132,26 +154,72 @@ function setRate(cff)
     softcut.rate(sample_voice, cffrate)
     softcut.rate(rec_voice, cffrate)
     print("CFF Rate:", cffrate)
+    redraw()
     return cffrate
 end
 
---filtering
--- turning both hp and lp filters on will have no effect.
--- if you want a band-pass then you'll need to activate post_filter_bp only and play with the centre freq and rq values.
--- if this doesn't lead to the desired effect you'll need to re-route softcut into supercollider and do the filtering there.
+function update_rate()
+  local n = math.pow(2, detune)
+  local cff = animal_tab[current_animal].cff
+  local new_rate = setRate(cff) * n
+  
+
+  softcut.rate(sample_voice, new_rate)
+  softcut.rate(rec_voice, new_rate)
+  
+  print("Rate with Detune:", new_rate)
+  redraw()
+end
+
+
 function setFreq()
-    local hp = animal_tab[current_animal].hp
-    local lp = animal_tab[current_animal].lp
+    local bp = animal_tab[current_animal].bp
+    local q = animal_tab[current_animal].q
     softcut.post_filter_dry(sample_voice, 0)
-    softcut.post_filter_hp(sample_voice, 1)
-    softcut.post_filter_lp(sample_voice, 1)
-    softcut.post_filter_fc(sample_voice, hp, lp) -- passing too many args here. post_filter_fc expects voice and freq_value
+    softcut.post_filter_bp(sample_voice, 1)
+    softcut.post_filter_rq (sample_voice, 1 / q)
+    softcut.post_filter_fc(sample_voice, bp)
+
     
     softcut.post_filter_dry(rec_voice, 0)
-    softcut.post_filter_hp(rec_voice, 1)
-    softcut.post_filter_lp(rec_voice, 1)
-    softcut.post_filter_fc(rec_voice, hp, lp) -- too many args (lp will be ignored)
+    softcut.post_filter_bp(rec_voice, 1)
+    softcut.post_filter_rq (rec_voice, 1 / q)
+    softcut.post_filter_fc(rec_voice, bp)
 end
+
+function updateFreqView()
+    local view_state = params:get("freq_view")
+    if view_state == 2 then
+        setFreq()
+    else
+        softcut.post_filter_dry(sample_voice, 1)
+        softcut.post_filter_dry(rec_voice, 1)
+    end
+end
+
+--shapeshifter function
+function shapeShifter()
+    if params:get("shape_shifter") == 2 then  -- Check if the shapeShifter is enabled
+        current_animal = math.random(1, #animal_tab)
+        setRate(animal_tab[current_animal].cff)
+        if params:get("freq_view") == 2 then
+            setFreq(animal_tab[current_animal].bp)
+        end
+        redraw()  -- Update the screen to show the new animal
+    end
+end
+
+
+function start_shape_shifter()
+    shape_shifter_metro.event = shapeShifter
+    shape_shifter_metro.time = params:get("shape_shifter_interval")
+    shape_shifter_metro:start()
+end
+
+function stop_shape_shifter()
+    shape_shifter_metro:stop()
+end
+
 
   
 -- load sample file
@@ -160,7 +228,7 @@ function load_audio(path)
       local ch, len = audio.file_info(path)
       if ch > 0 and len > 0 then
         softcut.buffer_clear_channel(2)
-        softcut.buffer_read_stereo(path, 0, 1, -1, 1, 2, 0, 1) -- needs to be mono if you need separate buffers this is why you're hearing the sample being overdubbed.
+        softcut.buffer_read_mono(path, 0, 1, -1, 1, 2, 0, 1)
         local l = math.min(len / 48000, MAX_BUFFER)
         softcut.loop_start(sample_voice, 1)
         softcut.loop_end(sample_voice, 1 + l)
@@ -184,51 +252,57 @@ function toggle_sample()
 end
 
 --UI--
-  
+
+
 function key(n, z)
-  if n == 1 and z == 1 then -- try to avoid K1 to do things other than use as a shift key.
-    rec = rec == 0 and 1 or 0
-    --play = play == 0 and 1 or 0
-    play = 1
-    print("Recording external audio:", rec == 1)
-    softcut.rec(1, rec) -- don't toggle flags. modulate levels insted.
-    softcut.play(1, play) -- don't toggle flags. set level and position instead.
+  if n == 1 then
+    k1_pressed = (z == 1)
   elseif n == 2 and z == 1 then
-    sample_is_planted = not sample_is_planted -- toggle the state before you apply conditionals, no?
-      if sample_is_planted then
-        softcut.rate(sample_voice, 0) -- this is intentionally stopping playback right?
-        softcut.rate(rec_voice, 0) -- bet it sounds cool with such a large slew rate.
-      else
-        setRate(animal_tab[current_animal].cff)
-      end
-  elseif n == 3 and z == 1 then
-    sample_is_planted = not sample_is_planted -- toggle the state before you apply conditionals, no?
-    if sample_is_planted then
-      setRate(animal_tab[current_animal].cff)
+    if k1_pressed then
+      rec = rec == 0 and 1 or 0
+      print("Recording external audio:", rec == 1)
+      softcut.rec_level(1, rec)
     else
-      setRate(-animal_tab[current_animal].cff) -- why this? you're neither stopping or reversing playback. just asking.
+      play = play == 0 and 1 or 0
+      sample_is_planted = play == 1
+      if play == 1 then
+        setRate(animal_tab[current_animal].cff)
+        print("stop/start")
+      else
+        -- Stop playback
+        softcut.rate(sample_voice, 0)
+        softcut.rate(rec_voice, 0)
+      end
+      print("Playback", play == 1 and "started" or "stopped")
     end
+  elseif n == 3 and z == 1 then
+     if k1_pressed then
+       softcut.buffer_clear(1)
+       else
+      sample_is_planted = not sample_is_planted
+        if sample_is_planted then
+          setRate(animal_tab[current_animal].cff)
+        else
+        setRate(-animal_tab[current_animal].cff) -- why this? you're neither stopping or reversing playback. just asking///...its reversing!
+        end
+      end
   end
 end
 
 
-
 -- encoder control
 function enc(n, d)
-  if n == 1 then
+    if n == 1 then
     current_animal = util.clamp(current_animal + d, 1, #animal_tab)
     setRate(animal_tab[current_animal].cff)
-    setFreq(animal_tab[current_animal].lp, animal_tab[current_animal].hp)
+    if params:get("freq_view") == 2 then
+      setFreq(animal_tab[current_animal].bp)
+    end
   elseif n == 2 then 
     params:delta("sample_level", d)
+    params:delta("rec_level", d)
   elseif n == 3 then
-    -- this will only ever give you two values because you're basically first setting the rate via setRate(CFF)
-    -- which also returns the current rate to which you add delta and then set the rate again.
-    -- if you need a detune parameter add this to params and include the variable in your setRate() function.
-    -- see the detune param in mlre if you want to add detune in cent steps.
-    local new_rate = setRate(animal_tab[current_animal].cff) + d
-    softcut.rate(sample_voice, new_rate)
-    print("rate is " .. new_rate)
+    params:delta("detune", d)
   end
   redraw()
 end
@@ -237,6 +311,7 @@ end
 --SCREEN
 function redraw()
   screen.clear()
+  screen.level(15)
   screen.font_size(15)
   screen.font_face(49)
   screen.move(5, 10)
@@ -248,6 +323,7 @@ function redraw()
   screen.font_size(8)
   screen.font_face(1)
   screen.move(5, 35)
+  screen.level(5)
   screen.text("Order: " .. animal_tab[current_animal].order)
   screen.move(5, 44)
   screen.text("CFF: " .. animal_tab[current_animal].cff)
